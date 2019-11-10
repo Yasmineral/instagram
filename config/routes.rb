bc8e5c8bdf54d571ddc89864db4f75a707bce040
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-11-10 17:24:51 +0000

    Add login & sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   devise_for :users
   # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-root 'users#index'
+
+  root 'welcome#index'
 # Changes the root route. State the controller and then the action
 end
